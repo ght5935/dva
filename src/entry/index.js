07fc53a4cb2b3570d1197fe0a90b1ecc51dcc646
gaohuatao <ght5935@163.com>
2018-08-14 15:16:25 +0800

    '优化'

--- src/entry/index.js
+++ src/entry/index.js
@@ -9,8 +9,6 @@ import {browserHistory} from 'dva/router';
 import { createLogger } from 'redux-logger';
 import '../index.css';
 
-import index from '../models/example';
-import router from '../routers/router';
 // 1. Initialize
 const app = dva();
 
@@ -18,10 +16,8 @@ const app = dva();
 app.use(createLoading());
 
 // 3. Model
-// app.model(require('../models/example'));
-app.model(index);
+app.model(require('../models/example').default);
 // 4. Router
-// app.router(require('../routers/router').default);
-app.router(router);
+app.router(require('../routers/router').default);
 // 5. Start
 app.start('#root');
